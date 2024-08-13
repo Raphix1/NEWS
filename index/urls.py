f4681cdb0b7e11a6d6b91498beba6d76e6eedc2b
Raphix1 <shhahzodd@gmail.com>
2024-08-13 07:51:18 +0500

    third

--- index/urls.py
+++ index/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home_page),
     path('category/', views.get_category),
+    path('news/', views.get_news),
 ]
--- index/views.py
+++ index/views.py
@@ -3,7 +3,15 @@ from django.views import View
 from .models import NewsCategory, News
 # Create your views here.
 def home_page(request):
-    return render(request, 'home.html')
+    view_news = News.objects.all()
+    view_categ = NewsCategory.objects.all()
+
+    context = {'view_news': view_news,
+               'view_categ': view_categ}
+    return render(request, 'home.html', context)
 
 def get_category(request):
     return render(request, 'category.html')
+
+def get_news(request):
+    return render(request, 'news.html')
