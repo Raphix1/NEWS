56951ecb80459d3bb8d10160fe2dd60823948acd
Raphix1 <shhahzodd@gmail.com>
2024-08-13 08:10:48 +0500

    fourth

--- index/admin.py
+++ index/admin.py
@@ -2,7 +2,12 @@ from django.contrib import admin
 from .models import NewsCategory, News
 
 # Register your models here.
-admin.site.register(NewsCategory)
-admin.site.register(News)
+@admin.register(NewsCategory)
+class CategoryAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+
+@admin.register(News)
+class ProductAdmin(admin.ModelAdmin):
+    search_fields = ['head']
 
 
