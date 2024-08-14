17f1a8f6317fc4305d08d7b8423108286ad83bd4
Raphix1 <shhahzodd@gmail.com>
2024-08-15 00:45:29 +0500

    reg

--- index/urls.py
+++ index/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('', views.home_page),
     path('category/', views.get_category),
     path('news/', views.get_news),
+    path('search', views.search_news),
+    path('register', views.Register.as_view()),
+    path('logout', views.logout_view),
 ]
--- index/views.py
+++ index/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
-from django.views import View
+from django.shortcuts import render, redirect
 from .models import NewsCategory, News
-# Create your views here.
+from django.views import View
+from django.contrib.auth import login, logout
+from django.contrib.auth.models import User
+from .forms import RegisterForm
+
 def home_page(request):
     view_news = News.objects.all()
     view_categ = NewsCategory.objects.all()
@@ -15,3 +18,43 @@ def get_category(request):
 
 def get_news(request):
     return render(request, 'news.html')
+
+def search_news(request):
+    if request.method == 'POST':
+        get_product = request.POST.get('search_news')
+
+        try:
+            exact_product = News.objects.get(pr_name__icontains=get_product)
+            return redirect(f'product/{exact_product.id}')
+        except:
+            print('не нашел')
+            return redirect('/')
+
+class Register(View):
+    template_name = 'registration/register.html'
+
+
+    def get(self, request):
+        context = {'form': RegisterForm}
+        return render(request, self.template_name, context)
+
+
+    def post(self, request):
+        form = RegisterForm(request.POST)
+
+        if form.is_valid():
+            username = form.clean_username()
+            password2 = form.cleaned_data.get("password2")
+            email = form.cleaned_data.get("email")
+            user = User.objects.create_user(username, password=password2, email=email)
+            print(username, password2, email)
+            user.save()
+            login(request, user)
+            return redirect('/')
+        context = {'form': RegisterForm}
+        return render(request, self.template_name, context)
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('/')
